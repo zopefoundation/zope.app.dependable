6a920c27fd995682127593775a647fa6f62d481d
Stephan Richter <stephan.richter@gmail.com>
2004-03-13 22:02:11 +0000

    Move dependable interfaces to zope.app.dependable.interfaces after making zope.app.dependable a package.

---
+++ interfaces.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: interfaces.py,v 1.1 2004/03/13 22:02:04 srichter Exp $
+"""
+
+from zope.interface import Interface
+
+class IDependable(Interface):
+    """Objects that other objects depend on.
+
+    Note that IDependable will normally be implemented by an adapter.
+    """
+
+    def addDependent(location):
+        """Add a dependency to a dependent object by location
+
+        The location is the physical path to the dependent object.
+        """
+    def removeDependent(location):
+        """Remove a dependency with a dependent object by location.
+
+        The location is the physical path to the dependent object.
+        """
+    def dependents():
+        """Return a sequence of dependent object locations.
+        """
+
+__doc__ = IDependable.__doc__ + __doc__
+
+
+"""
+$Id: interfaces.py,v 1.1 2004/03/13 22:02:04 srichter Exp $
+"""
+
+class DependencyError(Exception):
+    """ This object is dependable"""
